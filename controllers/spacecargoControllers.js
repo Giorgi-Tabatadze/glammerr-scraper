2086795d4fd3437826dfa68b9f2680488795a270
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2023-03-03 21:47:40 +0400

    run 1 account at a time instead of all together

--- controllers/spacecargoControllers.js
+++ controllers/spacecargoControllers.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-await-in-loop */
 const asyncHandler = require("express-async-handler");
 const spacecargoScraper = require("./utils/spacecargoScarper");
 
@@ -11,30 +13,30 @@ const createScrapingProcess = asyncHandler(async (req, res, next) => {
   res.status(200);
   res.json({ message: "Scraping process started" });
 
-  const updatedTrackings = [];
+  for (const scaccount of scaccounts) {
+    const updatedTrackings = [];
 
-  await Promise.all(
-    scaccounts.map(async (scaccount) => {
-      const accountTrackings = await spacecargoScraper.spacecargoScraper(
-        scaccount,
-      );
-      updatedTrackings.push(...accountTrackings);
-    }),
-  );
+    const accountTrackings = await spacecargoScraper.spacecargoScraper(
+      scaccount,
+    );
+    updatedTrackings.push(...accountTrackings);
+    const response = await fetch(
+      `${process.env.BACKEND_APP_DOMAIN}/scrapings`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          trackings: updatedTrackings,
+          secretToken: process.env.SCRAPER_SECRET_TOKEN,
+        }),
+      },
+    );
+    const data = await response.json();
 
-  const response = await fetch(`${process.env.BACKEND_APP_DOMAIN}/scrapings`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      trackings: updatedTrackings,
-      secretToken: process.env.SCRAPER_SECRET_TOKEN,
-    }),
-  });
-  const data = await response.json();
-
-  console.log(data);
+    console.log(data);
+  }
 
   next();
 });
